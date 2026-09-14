ce6ab0011d477f03123aa3152de55c6c78599573
gottaminesomething <62343785+gottaminesomething@users.noreply.github.com>
2026-09-14 14:51:34 -0400

    Update tasktrack.py
    update add task so that it actually does something, specifically adding an input to a list and printing that it did it

--- tasktrack.py
+++ tasktrack.py
@@ -15,8 +15,11 @@ def display_menu():
 
 def add_task(tasks):
     """Prompt the user for a task and add it to the task list."""
-    #TODO: Complete this function in Step 8.
-    pass
+    task = input("Enter a new task: ")
+    #TODO: Use append() to add task to the tasks list.
+    tasks.append(task)
+    #TODO: Print "Task added successfully."
+    print("Task Added Successfully")
 
 
 def view_tasks(tasks):

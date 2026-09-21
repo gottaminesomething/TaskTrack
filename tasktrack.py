99d29f19752afa1ba86d280e6bf62bad67bc000b
gottaminesomething <62343785+gottaminesomething@users.noreply.github.com>
2026-09-21 15:40:13 -0400

    reject empty task input

--- tasktrack.py
+++ tasktrack.py
@@ -15,7 +15,11 @@ def display_menu():
 
 def add_task(tasks):
     """Prompt the user for a task and add it to the task list."""
-    task = input("Enter a new task: ")
+    task = input("Enter a new task: ").strip()
+    if not task:
+            print("not a valid task, meanie")
+            return
+    
     #TODO: Use append() to add task to the tasks list.
     tasks.append(task)
     #TODO: Print "Task added successfully."
